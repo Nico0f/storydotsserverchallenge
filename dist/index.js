92c9de3b8484b17f846c97152364e0bbe12e2bad
nico0f <franconicoletti.14@gmail.com>
2023-05-05 18:05:46 -0300

    first

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const port = 3001;
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const port = Number(process.env.PORT) || 3000;
 app_1.app.get('/', async (req, res) => {
     res.send('Hello World');
 });
 app_1.app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express'
 import prisma from './prisma'
 import { app } from './app'
+import { config } from "dotenv";
+config();
 
-const port: number = 3001
+const port: number = Number(process.env.PORT) || 3000
 
 app.get('/', async (req, res) => {
   res.send('Hello World')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server listening on port ${port}`)
 })
